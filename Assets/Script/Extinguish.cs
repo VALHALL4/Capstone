602c80491ffd6d96ed9e67ca655d7ed03c0932d1
VALHALL4 <hsm011007@naver.com>
2024-03-16 23:01:49 +0900

    소화기 불끄는 기능 수정 및 소리 추가
    소리는 나도록 추가는 했는데 소리가 반복해서 나는 부분이 매끄럽지는 못함 조금 더 찾아봐야할듯함

--- Assets/Script/Extinguish.cs
+++ Assets/Script/Extinguish.cs
@@ -11,7 +11,7 @@ public class Extinguish : MonoBehaviour
         {
             Debug.Log("접촉했습니다.");
             fire = other.GetComponent<ParticleSystem>();
-            fire.Stop(); //particle system일경우 파티클 재생을 멈추기
+            //fire.Stop(); //particle system일경우 파티클 재생을 멈추기
         }
     }
 }
--- Assets/Script/FireExtinguisher.cs
+++ Assets/Script/FireExtinguisher.cs
@@ -8,16 +8,20 @@ public class FireExtinguisher : MonoBehaviour
     [SerializeField] private Transform spraypos;
     public ParticleSystem spray;
     public Slider gaugeSlider; // 게이지 UI
+    private AudioSource audiosource;
 
     private XRGrabInteractable grabInteractable;
 
     public float maxGauge = 100f; // 최대 게이지 양
     public float gaugeConsumptionRate = 20f; // 게이지 사용 속도
     private float currentGauge; // 현재 게이지 양
-    private bool isFiring; // 분사 중인지 여부
+    private bool isFiring=false; // 분사 중인지 여부
+
+    public float rayLength = 10f;
 
     void Start()
     {
+        audiosource = GetComponent<AudioSource>();
         grabInteractable = GetComponent<XRGrabInteractable>();
         grabInteractable.activated.AddListener(x => StartSpray());
         grabInteractable.deactivated.AddListener(x => StopSpray());
@@ -26,8 +30,9 @@ public class FireExtinguisher : MonoBehaviour
 
     void Update()
     {
-       
-      
+        if (isFiring)
+            Raycastcheck();
+    
         
     }
 
@@ -39,20 +44,30 @@ public class FireExtinguisher : MonoBehaviour
         }
     }
 
-
-    
-
     public void StartSpray()
-    {      
-       spray.Play();
-       UpdateGauge();
+    {
+        spray.Play();
+        audiosource.Play();
+        isFiring = true;
     }
     public void StopSpray()
     {
         spray.Stop();
+        audiosource.Stop();
+        isFiring = false;
     }
 
 
+    void Raycastcheck()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(spraypos.position, spraypos.up, out hit, rayLength, LayerMask.GetMask("Fire")))
+        {       
+            //hit.transform.gameObject.SendMessage()    
+            Destroy(hit.collider.gameObject);
+
+        }
+    }
 
     private void UpdateGauge()
     {
