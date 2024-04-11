f7763732f97e423821cb2e1a5dbf0416f52dc614
VALHALL4 <hsm011007@naver.com>
2024-04-12 02:39:49 +0900

    소화기 체험관
    1. 에셋 추가후 체험관 디자인 변경함
    2. 한 테이블에서 소화기와 투척용 소화기 모두 체험
    3. 투척용 소화기가 현재 작동을 안함 왜 안되는지는 아직 모르겠음

--- Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
+++ Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
@@ -5,19 +5,25 @@ using UnityEngine.UI;
 using TMPro;
 public class FEchangeText : MonoBehaviour
 {
-   public TextMeshProUGUI instructionText;
+    public TextMeshProUGUI instructionText;
+    public GameObject TFE;
+    public Transform TFErespawnpos;
+    public GameObject Fire;
+    public Transform Firerespawnpos;
 
     private void OnEnable()
     {
         FireExtinguisher.OnFireExtinguisherGrabbed += ChangeTextToSafetyPin;
         PullOutPin.OnSafetyPinPulled += ChangeTextToStartSpraying;
         FireExtinguisher.OnStartSpraying += ChangeTextToEnd;
+        ThrowFireExitinguisher.OnFireExtinguisherThrow += ChangeTextTFE;
     }
     private void OnDisable()
     {
         FireExtinguisher.OnFireExtinguisherGrabbed -= ChangeTextToSafetyPin;
         PullOutPin.OnSafetyPinPulled -= ChangeTextToStartSpraying;
         FireExtinguisher.OnStartSpraying -= ChangeTextToEnd;
+        ThrowFireExitinguisher.OnFireExtinguisherThrow -= ChangeTextTFE;
     }
 
     private void Start()
@@ -34,12 +40,28 @@ public class FEchangeText : MonoBehaviour
     private void ChangeTextToStartSpraying()
     {
         instructionText.text = "불을 향하여 호스를 조준하고 버튼을 눌러 분사하세요.";
+        
     }
 
     // 분사를 시작할 때 호출되는 메서드
+
     private void ChangeTextToEnd()
     {
-        instructionText.text = "잘했습니다. 소화기 체험이 완료되었습니다.";
+        instructionText.text = "잘했습니다. 소화기 체험이 완료되었습니다.";       
+        Invoke("ChangeTextStartTFE", 3.0f);
+    }
+
+    private void ChangeTextStartTFE()
+    {
+        instructionText.text = "이번에는 투척용 소화기를 체험해보겠습니다.";
+        Instantiate(TFE, TFErespawnpos);
+        Instantiate(Fire, Firerespawnpos);
+    }
+    private void ChangeTextTFE()
+    {
+        instructionText.text = "투척용 소화기는 사용법이 간단하여\n 몸이 불편하신분들이나 노인, \n 어린이등 모두가 손쉽게 사용할 수 있습니다.";
+
+
     }
 
 }
