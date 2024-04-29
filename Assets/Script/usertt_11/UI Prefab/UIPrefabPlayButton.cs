7ce768032eb27d94d63e910085e5889438b68cf0
user00patcr <159416750+user00patcr@users.noreply.github.com>
2024-04-29 22:42:07 +0900

    손전등 체험관
    손전등 체험관 제작

--- Assets/Script/usertt_11/UI Prefab/UIPrefabPlayButton.cs
+++ Assets/Script/usertt_11/UI Prefab/UIPrefabPlayButton.cs
@@ -9,21 +9,55 @@ public class UIPrefabPlayButton : MonoBehaviour
    public GameObject gameObject3;
    public GameObject gameObject4;
 
-
+    public GameObject gameObject5;
     public Material mat1;
     public Material mat2;
     public Material mat3;
 
+    public GameObject[] gameObject6 = new GameObject[6];
+    public GameObject ob1;
+    public GameObject ob2;
+    public GameObject ob3;
+    public GameObject ob4;
+    public GameObject ob5;
+    public GameObject ob6;
+
+    public Material omat1;
+    public Material omat2;
+    public Material omat3;
+    public Material omat4;
+    public Material omat5;
+    public Material omat6;
+
     public void ButtonClick()
     {
-        for (int i = 0; i < 27; i++)
+        for (int i = 0; i < 20; i++)
         {
             gameObject1[i].GetComponent<MeshRenderer>().material = mat1;
         }
-     
-        gameObject2.GetComponent<MeshRenderer>().material = mat2;
-        gameObject3.GetComponent<MeshRenderer>().material = mat3;
+        for (int i = 20; i < 27; i++)
+        {
+            gameObject1[i].GetComponent<MeshRenderer>().material = mat2;
+        }
+        for (int i = 0; i < 6; i++)
+        {
+            gameObject6[i].SetActive(true);
+        }
+        gameObject2.GetComponent<MeshRenderer>().material = mat1;
+        gameObject3.GetComponent<MeshRenderer>().material = mat1;
+        gameObject5.GetComponent<MeshRenderer>().material = mat3;
         gameObject4.SetActive(true);
+
+        ob1.GetComponent<MeshRenderer>().material = omat1;
+        ob2.GetComponent<MeshRenderer>().material = omat2;
+        ob3.GetComponent<MeshRenderer>().material = omat3;
+
+
+        ob4.GetComponent<MeshRenderer>().material = omat4;
+        ob5.GetComponent<MeshRenderer>().material = omat5;
+        ob6.GetComponent<MeshRenderer>().material = omat6;
+
+
         RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Trilight;
 
     }
