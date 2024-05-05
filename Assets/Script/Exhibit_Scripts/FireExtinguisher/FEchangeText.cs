ac7fa280fd901564afd0f2070ea8fe4f59a1ffdf
leesh8258 <102220481+leesh8258@users.noreply.github.com>
2024-05-05 17:00:04 +0900

    소화기 씬 텍스트 크기 및 글 수정
    -후에 소화기씬 각 소화기 정보 기입할 예정 (소화기 정보 UI 작업)

--- Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
+++ Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
@@ -34,33 +34,33 @@ public class FEchangeText : MonoBehaviour
 
     private void ChangeTextToSafetyPin()
     {
-        instructionText.text = "2. 소화기 손잡이 부분에 위치한 안전핀을 뽑아주세요";
+        instructionText.text = "소화기 손잡이 부분에 위치한 안전핀을 뽑아주세요";
     }
 
     // 안전핀을 뽑았을 때 호출되는 메서드
     private void ChangeTextToStartSpraying()
     {
-        instructionText.text = "3. 불을 향해 호스를 조준하고 버튼을 눌러 분사하세요";
+        instructionText.text = "불을 향해 호스를 조준하고 버튼을 눌러 분사하세요";
     }
 
     // 분사를 시작할 때 호출되는 메서드
 
     private void ChangeTextToEnd()
     {
-        instructionText.text = "잘하셨습니다. 다음은 투척용소화기에 대해 설명해드리겠습니다";       
+        instructionText.text = "다음은 투척용소화기를 사용하겠습니다";       
         Invoke("ChangeTextStartTFE", 3.0f);
     }
 
     private void ChangeTextStartTFE()
     {
-        instructionText.text = "투척용 소화기는 준비과정이 필요없이 화재가 난 곳에 던지면 되는 특수 소화기입니다 투척용 소화기를 들고 불을 향해 던져보세요";
+        instructionText.text = "투척용 소화기는 준비과정 필요없이 사용이 가능한 특수 소화기입니다. 투척용 소화기를 들고 불을 향해 던져보세요";
         Instantiate(TFE, TFErespawnpos);
         Instantiate(Fire, Firerespawnpos);
     }
 
     private void ChangeTextTFE()
     {
-        instructionText.text = "잘하셨습니다! 이처럼 투척용 소화기는 사용법이 간단하여 몸이 불편하신분들이나 노인, 어린이등 모두가 손쉽게 사용할 수 있습니다";
+        instructionText.text = "두 소화기 모두 기능은 동일하지만 사용방법과 효과에 차이가 존재합니다. 각각의 소화기 정보는 앞에있는 정보창에서 읽어보시길 바랍니다";
     }
 
 }
