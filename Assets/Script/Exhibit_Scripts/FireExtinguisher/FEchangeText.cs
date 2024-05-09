554b146eb1e805336171d6ceeb9b7419a9525cf3
leesh8258 <102220481+leesh8258@users.noreply.github.com>
2024-05-10 01:52:56 +0900

    소화기 체험관 내레이션, UI 추가

--- Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
+++ Assets/Script/Exhibit_Scripts/FireExtinguisher/FEchangeText.cs
@@ -14,6 +14,11 @@ public class FEchangeText : MonoBehaviour
     public GameObject firstPreview;
     public GameObject secondPreview;
 
+    public AudioClip[] audioClips;
+    private AudioSource audioSource;
+    private int audioIndex = 0;
+    public GameObject canvas;
+
     private void OnEnable()
     {
         FireExtinguisher.OnFireExtinguisherGrabbed += ChangeTextToSafetyPin;
@@ -32,7 +37,18 @@ public class FEchangeText : MonoBehaviour
     private void Start()
     {
         instructionText.color = new Color32(0, 0, 0, 255);
+        audioSource = GetComponent<AudioSource>();
         
+    }
+
+    private void playAudio()
+    {
+        if(audioIndex < audioClips.Length)
+        {
+            audioSource.clip = audioClips[audioIndex];
+            audioSource.Play();
+            audioIndex++;
+        }
     }
 
     private void ChangeTextToSafetyPin()
@@ -40,6 +56,7 @@ public class FEchangeText : MonoBehaviour
         firstPreview.SetActive(false);
         secondPreview.SetActive(true);
         instructionText.text = "소화기 손잡이 부분에 위치한 안전핀을 뽑아주세요";
+        playAudio();
     }
 
     // 안전핀을 뽑았을 때 호출되는 메서드
@@ -47,6 +64,7 @@ public class FEchangeText : MonoBehaviour
     {
         secondPreview.SetActive(false);
         instructionText.text = "불을 향해 호스를 조준하고 버튼을 눌러 분사하세요";
+        playAudio();
     }
 
     // 분사를 시작할 때 호출되는 메서드
@@ -55,6 +73,7 @@ public class FEchangeText : MonoBehaviour
     {
         instructionText.text = "다음은 투척용소화기를 사용하겠습니다";       
         Invoke("ChangeTextStartTFE", 3.0f);
+        playAudio();
     }
 
     private void ChangeTextStartTFE()
@@ -62,11 +81,14 @@ public class FEchangeText : MonoBehaviour
         instructionText.text = "투척용 소화기는 준비과정 필요없이 사용이 가능한 특수 소화기입니다. 투척용 소화기를 들고 불을 향해 던져보세요";
         Instantiate(TFE, TFErespawnpos);
         Instantiate(Fire, Firerespawnpos);
+        playAudio();
     }
 
     private void ChangeTextTFE()
     {
         instructionText.text = "두 소화기 모두 기능은 동일하지만 사용방법과 효과에 차이가 존재합니다. 각각의 소화기 정보는 앞에있는 정보창에서 읽어보시길 바랍니다";
+        playAudio();
+        canvas.SetActive(true);
     }
 
 }
