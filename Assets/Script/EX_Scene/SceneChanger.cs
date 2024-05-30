8689328043d7e530c8f43d905871a94773f8a76e
VALHALL4 <hsm011007@naver.com>
2024-05-30 16:06:39 +0900

    튜토리얼 씬 ,전시관 씬 , 체험 씬 수정
    튜토리얼 씬: 발판 화살표로 변경
    전시관 씬: 체험씬, 대피구역씬 앞에 이쓴 발판 콜리더 삭제
    체험 씬: 씬이동시 나오는 ui 수정

--- Assets/Script/EX_Scene/SceneChanger.cs
+++ Assets/Script/EX_Scene/SceneChanger.cs
@@ -15,7 +15,7 @@ public class SceneChanger : MonoBehaviour
     public string sceneName;
     public GameObject canvas;
     public TextMeshProUGUI instructionText;
-    public string[] txt = { "수고하셨습니다.실제 대피 체험을 완료하셨습니다", "잠시 후 전시관으로 이동합니다."};
+   
     public float delay = 2.0f;
 
 
@@ -32,23 +32,23 @@ public class SceneChanger : MonoBehaviour
     private IEnumerator LoadSceneAfterDelay()
     {
         canvas.SetActive(true);
+        instructionText.color = new Color32(0,0,0, 255);
         instructionText.text = "수고하셨습니다.실제 대피 체험을 완료하셨습니다";
-        instructionText.color = new Color32(255, 255, 255, 255);
         yield return new WaitForSeconds(3.0f);
         instructionText.text = "잠시 후 전시관으로 이동합니다.";
-        this.canvasGo.SetActive(true);
-        this.StartCoroutine(CoFadeOut());
+        canvasGo.SetActive(true);
+        StartCoroutine(CoFadeOut());
 
     }
 
     private IEnumerator CoFadeOut()
     {
-        Color color = this.dim.color;
+        Color color = dim.color;
 
         while (true)
         {
             color.a += 0.01f;
-            this.dim.color = color;
+            dim.color = color;
 
             if (color.a >= 1)
             {
